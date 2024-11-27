ee2757b0535fa4624b4f0217826f707ee358dff3
CecildeSist <arcetwo.com@gmail.com>
2024-11-27 13:24:06 -0500

    19 tasks left today

--- app/src/main/java/com/example/cecil_finalproject/ConfirmDeleteActivity.java
+++ app/src/main/java/com/example/cecil_finalproject/ConfirmDeleteActivity.java
@@ -1,6 +1,9 @@
 package com.example.cecil_finalproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,43 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ConfirmDeleteActivity extends AppCompatActivity {
+    TextView tJCon_hold, tJCon_this, tJCon_1, tJCon_2, tJCon_3, tJCon_know;
+    Button bJCon_y, bJCon_n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_confirm_delete);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //Connect GUI elements
+        tJCon_hold = findViewById(R.id.txtVConfirmDelete_holdOn);
+        tJCon_this = findViewById(R.id.txtVConDel_thisIsWhat);
+        tJCon_1 = findViewById(R.id.txtVConfirm_1);
+        tJCon_2 = findViewById(R.id.txtVConfirm_2);
+        tJCon_3 = findViewById(R.id.txtVConfirm_3);
+        tJCon_know = findViewById(R.id.txtVConfirm_still);
+        bJCon_y = findViewById(R.id.btnVConfirm_yes);
+        bJCon_n = findViewById(R.id.btnVConfirm_no);
+
+        //Set the text NOTE TO SELF not done
+
+        //Call button listeners NOTE TO SELF not done
+        conYesListener();
+    }
+
+    //NOTE TO SELF not done
+    private void conYesListener() {
+        bJCon_y.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
         });
+    }
+
+    //NOTE TO SELF not done
+    private void conNoListener() {
+
     }
 }
