4db020d9854fee093269dc6a0c24d74d9cb1fb12
CecildeSist <arcetwo.com@gmail.com>
2024-11-27 13:51:32 -0500

    18 tasks left today

--- app/src/main/java/com/example/cecil_finalproject/LeaveReviewActivity.java
+++ app/src/main/java/com/example/cecil_finalproject/LeaveReviewActivity.java
@@ -1,6 +1,10 @@
 package com.example.cecil_finalproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,56 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LeaveReviewActivity extends AppCompatActivity {
+    TextView tJLeave_choU, tJLeave_num;
+    SeekBar seekJLeave;
+    Button bJLeave_rev, bJLeave_back;
+
+    DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_leave_review);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //Connect GUI elements
+        tJLeave_choU = findViewById(R.id.txtVLeaveReview);
+        tJLeave_num = findViewById(R.id.txtVLeaveReview_number);
+        seekJLeave = findViewById(R.id.seekVLeaveReview_score);
+        bJLeave_rev = findViewById(R.id.btnVLeaveReview_review);
+        bJLeave_back = findViewById(R.id.btnVLeaveReview_back);
+
+        //Init dbHelper
+        dbHelper = new DatabaseHelper(this);
+        dbHelper.initAllTables();
+
+        //Call seekbar and button listeners NOTE TO SELF not done
+        seekBarListener();
+        leaveReviewListener();
+        leaveLeaveListener();
+    }
+
+    //NOTE TO SELF not done
+    private void seekBarListener() {
+    }
+
+    //NOTE TO SELF not done
+    private void leaveReviewListener() {
+        bJLeave_rev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    //NOTE TO SELF not done
+    private void leaveLeaveListener() {
+        bJLeave_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
         });
     }
 }
