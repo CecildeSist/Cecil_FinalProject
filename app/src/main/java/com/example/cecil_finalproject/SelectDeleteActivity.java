889cd2853120a4c8acaca6677b4ac362f4a3cbe4
CecildeSist <arcetwo.com@gmail.com>
2024-12-08 10:53:33 -0500

    9 tasks left today

--- app/src/main/java/com/example/cecil_finalproject/SelectDeleteActivity.java
+++ app/src/main/java/com/example/cecil_finalproject/SelectDeleteActivity.java
@@ -3,6 +3,8 @@ package com.example.cecil_finalproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -10,10 +12,16 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class SelectDeleteActivity extends AppCompatActivity {
     TextView txtJSelectDelete;
     Spinner spnJSelectDelete;
     Button btnJSelectDelete_proceed, btnJSelectDelete_back;
+
+    ArrayList<String> deleteChoices = new ArrayList<>();
+    ArrayAdapter<String> choicesAdapter;
+    String deleteChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +40,31 @@ public class SelectDeleteActivity extends AppCompatActivity {
         String loggedUser = (String) cameFrom.getSerializableExtra("Username:");
         txtJSelectDelete.setText(loggedUser + ", what do you want to delete?");
 
+        //Populate the spinner
+        deleteChoices.add("A review I made");
+        deleteChoices.add("A team I made");
+        deleteChoices.add("My entire account");
+        choicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, deleteChoices);
+        spnJSelectDelete.setAdapter(choicesAdapter);
+        spinnerListener();
+
         //Call button listeners
         selectDeleteProceedListener();
         selectDeleteBackListener();
+    }
+
+    private void spinnerListener() {
+        spnJSelectDelete.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                deleteChoice = adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     //NOTE TO SELF not done
